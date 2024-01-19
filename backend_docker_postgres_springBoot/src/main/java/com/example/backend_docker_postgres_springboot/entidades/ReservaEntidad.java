69b872a70cb25a7ea27cf81493b26621287ccb8c
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:48:05 -0500

    Update ReservaEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/ReservaEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/ReservaEntidad.java
@@ -13,4 +13,15 @@ public class ReservaEntidad {
     private String detalle_pasajero;
     private String num_asientos_reservados;
     private Boolean estado_reserva;
+
+    //muchos a uno con usuariso
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario")
+    private UsuarioEntidad id_usuario;
+
+
+    //muchos a uno con id horario
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_horario", referencedColumnName = "id_horario")
+    private HorarioEntidad id_horario;
 }
