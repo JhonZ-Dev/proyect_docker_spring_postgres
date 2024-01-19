401ca36214e661b1e78fccf05000781fbcd5995c
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:56:24 -0500

    Update RutaEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/RutaEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/RutaEntidad.java
@@ -3,6 +3,8 @@ package com.example.backend_docker_postgres_springboot.entidades;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Table(name ="tbl_rutas")
 @Data
@@ -12,5 +14,14 @@ public class RutaEntidad {
     private Long id_ruta;
     private String descripcion;
     private String duracion_estimada;
+
+    //relacion de uno a mcuhos con horario
+    @OneToMany(mappedBy = "id_horario",cascade = CascadeType.ALL)
+    private List<HorarioEntidad> horarioEntidads;
+
+    //relacion de muchos a uno con estacion
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_estacion", referencedColumnName = "id_estacion")
+    private EstacionEntidad id_estacion;
 
 }
