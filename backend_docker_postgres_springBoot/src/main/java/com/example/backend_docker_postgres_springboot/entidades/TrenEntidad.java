94cdec61d630e0b0ec544cc10c045bd498505bde
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:20:35 -0500

    Update TrenEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/TrenEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/TrenEntidad.java
@@ -1,4 +1,16 @@
 package com.example.backend_docker_postgres_springboot.entidades;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_trenes")
+@Data
 public class TrenEntidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id_tren;
+    private String modelo_tren;
+    private String capacidad;
+    private Boolean estado;
 }
