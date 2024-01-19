4dbaa21158154ccc71b61608d53e10236e2418e0
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:33:22 -0500

    Create EstacionService.java

---
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/servicios/EstacionService.java
@@ -0,0 +1,12 @@
+package com.example.backend_docker_postgres_springboot.servicios;
+
+import com.example.backend_docker_postgres_springboot.repositorios.EstacionRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstacionService {
+    @Autowired
+    public EstacionRepositorio repositorio;
+    
+}
