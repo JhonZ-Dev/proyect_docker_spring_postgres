6dfa26fd56115a4807c28299ae1be152790a82b9
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:15:45 -0500

    Update HorarioEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/HorarioEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/HorarioEntidad.java
@@ -1,9 +1,23 @@
 package com.example.backend_docker_postgres_springboot.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "tbl_horarios")
+@Data
 public class HorarioEntidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id_horario;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dias_semana;
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime hora_salida;
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime hora_llegada;
 }
