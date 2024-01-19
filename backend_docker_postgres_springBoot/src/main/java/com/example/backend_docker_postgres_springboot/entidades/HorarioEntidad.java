82a532c4c6fe23a1512da285da3a75186c9a198c
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:14:49 -0500

    Update HorarioEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/HorarioEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/HorarioEntidad.java
@@ -1,4 +1,9 @@
 package com.example.backend_docker_postgres_springboot.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_horarios")
 public class HorarioEntidad {
 }
