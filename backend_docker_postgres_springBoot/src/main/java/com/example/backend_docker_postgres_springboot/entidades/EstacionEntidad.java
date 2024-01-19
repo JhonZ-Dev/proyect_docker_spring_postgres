f3c7466fdba4a89ed974ac9fe077e9dddd9a6450
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 20:48:28 -0500

    Update EstacionEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/EstacionEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/EstacionEntidad.java
@@ -1,6 +1,20 @@
 package com.example.backend_docker_postgres_springboot.entidades;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Table(name = "tbls_estaciones")
+@Data
 public class EstacionEntidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id_estacion;
+    private String nombre_estacion;
+    private String ubicacion_estacion;
+    private String plataformas_estacion;
+    private String estacion_origen;
+    private String estacion_destino;
+
 
 }
