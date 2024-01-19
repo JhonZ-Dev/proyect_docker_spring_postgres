d0966bf61769c9a5cfcad16172462b1147e985a1
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 20:46:55 -0500

    CreateProyect

---
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/BackendDockerPostgresSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_docker_postgres_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDockerPostgresSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDockerPostgresSpringBootApplication.class, args);
+    }
+
+}
---
+++ backend_docker_postgres_springBoot/src/test/java/com/example/backend_docker_postgres_springboot/BackendDockerPostgresSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_docker_postgres_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDockerPostgresSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
