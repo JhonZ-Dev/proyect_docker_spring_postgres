a0765bd1c8103a50876ecdc44f15145caeababa9
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:28:48 -0500

    Update EstacionRepositorio.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/repositorios/EstacionRepositorio.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/repositorios/EstacionRepositorio.java
@@ -1,7 +1,10 @@
 package com.example.backend_docker_postgres_springboot.repositorios;
 
 import com.example.backend_docker_postgres_springboot.entidades.EstacionEntidad;
-import org.springframework.data.jpa.repository.JpaRepository;
+import
+        org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstacionRepositorio extends JpaRepository<EstacionEntidad, Long> {
 }
