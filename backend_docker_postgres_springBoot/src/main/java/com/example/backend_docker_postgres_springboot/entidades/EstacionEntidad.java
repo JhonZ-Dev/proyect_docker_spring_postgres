e0621e48ef0ec22142feef2e9060fda1969882dd
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:58:26 -0500

    Update EstacionEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/EstacionEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/EstacionEntidad.java
@@ -3,6 +3,8 @@ package com.example.backend_docker_postgres_springboot.entidades;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tbl_estaciones")
 @Data
@@ -15,6 +17,15 @@ public class EstacionEntidad {
     private String plataformas_estacion;
     private String estacion_origen;
     private String estacion_destino;
+
+    //relacion de uno a muchos con ruta
+    @OneToMany(mappedBy = "id_ruta",cascade = CascadeType.ALL)
+    private List<RutaEntidad> rutaEntidads;
+
+    //relacion de uno a muchos con tren
+    @OneToMany(mappedBy = "id_tren", cascade = CascadeType.ALL)
+    private List<TrenEntidad> trenEntidads;
+    
 
 
 }
