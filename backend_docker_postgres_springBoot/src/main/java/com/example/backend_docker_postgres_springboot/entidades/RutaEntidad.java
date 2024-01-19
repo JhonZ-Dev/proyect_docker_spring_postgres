eb19b6b2b3fca6f3b1cdffa7e7af41a52f7543a5
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:20:45 -0500

    Update RutaEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/RutaEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/RutaEntidad.java
@@ -1,9 +1,11 @@
 package com.example.backend_docker_postgres_springboot.entidades;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name ="tbl_rutas")
+@Data
 public class RutaEntidad {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
