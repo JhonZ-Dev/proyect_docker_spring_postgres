c120c7c27a1c5e128fa885ae0a5a855b32dd425f
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:28:45 -0500

    Update HorarioRepositorio.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/repositorios/HorarioRepositorio.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/repositorios/HorarioRepositorio.java
@@ -2,6 +2,8 @@ package com.example.backend_docker_postgres_springboot.repositorios;
 
 import com.example.backend_docker_postgres_springboot.entidades.HorarioEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HorarioRepositorio extends JpaRepository<HorarioEntidad, Long> {
 }
