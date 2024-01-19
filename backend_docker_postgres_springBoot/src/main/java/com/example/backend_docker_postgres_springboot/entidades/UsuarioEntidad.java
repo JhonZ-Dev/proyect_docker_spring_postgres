837f67286825c4cb431e7d82a05bc15166b60059
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:21:49 -0500

    Update UsuarioEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/UsuarioEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/UsuarioEntidad.java
@@ -1,4 +1,13 @@
 package com.example.backend_docker_postgres_springboot.entidades;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_usuarios")
+@Data
 public class UsuarioEntidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+
 }
