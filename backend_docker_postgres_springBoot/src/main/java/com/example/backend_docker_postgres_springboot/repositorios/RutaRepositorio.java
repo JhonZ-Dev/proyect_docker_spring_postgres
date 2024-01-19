6af34de1053d73fe50edee0e093e64e35db76bb8
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:30:08 -0500

    Updates

---
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/repositorios/RutaRepositorio.java
@@ -0,0 +1,10 @@
+package com.example.backend_docker_postgres_springboot.repositorios;
+
+import com.example.backend_docker_postgres_springboot.entidades.RutaEntidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RutaRepositorio extends JpaRepository<RutaEntidad, Long> {
+    
+}
