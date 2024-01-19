9d75ca61938c9ef1e5006cfee367e58a0c599984
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-18 21:16:52 -0500

    Update ReservaEntidad.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/ReservaEntidad.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/entidades/ReservaEntidad.java
@@ -1,4 +1,17 @@
 package com.example.backend_docker_postgres_springboot.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_reservas")
+@Data
 public class ReservaEntidad {
+    @Id
+    private Long id_reserva;
+    private String detalle_pasajero;
+    private String num_asientos_reservados;
+    private Boolean estado_reserva;
 }
