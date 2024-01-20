e15a40abfeb88815e53085df98500e92235c4efa
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-19 21:04:49 -0500

    Update RutaController.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/controladores/RutaController.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/controladores/RutaController.java
@@ -1,4 +1,50 @@
 package com.example.backend_docker_postgres_springboot.controladores;
 
+import com.example.backend_docker_postgres_springboot.entidades.RutaEntidad;
+import com.example.backend_docker_postgres_springboot.servicios.RutaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/ruta")
 public class RutaController {
+
+    @Autowired
+    public RutaService service;
+
+
+    //guardar
+    @PostMapping("/guardar/{id_estacion}")
+    public RutaEntidad guardarRuta(@RequestBody RutaEntidad rutaEntidad,
+                                   @PathVariable Long id_estacion){
+        return service.guardar(rutaEntidad, id_estacion);
+    }
+
+    //editar
+    @PutMapping("/guardar/{id_ruta}")
+    public RutaEntidad editarRuta(@RequestBody RutaEntidad rutaEntidad,
+                                   @PathVariable Long id_ruta){
+        return service.editar(rutaEntidad, id_ruta);
+    }
+    //traerTodas
+    @GetMapping("/listarTodos")
+    public List<RutaEntidad> traerTodos(){
+        return service.listar();
+    }
+
+    //eliminar
+    @DeleteMapping("/eliminar/{id_ruta}")
+    public void eliminar(@PathVariable Long id_ruta){
+        service.eliminar(id_ruta);
+    }
+
+    //taerPorId
+    @GetMapping("/traer/{id_ruta}")
+    public Optional<RutaEntidad> traerPorId(@PathVariable Long id_ruta){
+        return service.traerPorId(id_ruta);
+    }
+
 }
