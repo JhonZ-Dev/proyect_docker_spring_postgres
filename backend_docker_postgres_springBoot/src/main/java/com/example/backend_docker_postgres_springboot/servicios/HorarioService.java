03588c52513663965a2f145389ed981615b42565
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-19 20:29:53 -0500

    Update HorarioService.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/servicios/HorarioService.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/servicios/HorarioService.java
@@ -1,7 +1,11 @@
 package com.example.backend_docker_postgres_springboot.servicios;
 
 import com.example.backend_docker_postgres_springboot.entidades.HorarioEntidad;
+import com.example.backend_docker_postgres_springboot.entidades.RutaEntidad;
+import com.example.backend_docker_postgres_springboot.entidades.TrenEntidad;
 import com.example.backend_docker_postgres_springboot.repositorios.HorarioRepositorio;
+import com.example.backend_docker_postgres_springboot.repositorios.RutaRepositorio;
+import com.example.backend_docker_postgres_springboot.repositorios.TrenRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +17,34 @@ public class HorarioService {
     @Autowired
     public HorarioRepositorio repositorio;
 
+    @Autowired
+    public RutaRepositorio rutaRepositorio;
+    @Autowired
+    public TrenRepositorio trenRepositorio;
+
     //metodo para guardar
+    public HorarioEntidad guardarConTrenYRuta(HorarioEntidad horarioEntidad,
+                                              Long id_ruta,
+                                              Long id_tren){
+        //optener la ruta y el tren por su ID
+        Optional<RutaEntidad> rutaEntidadOptional = rutaRepositorio.findById(id_ruta);
+        Optional<TrenEntidad> trenEntidadOptional = trenRepositorio.findById(id_tren);
+        //verificar si existe
+        if(rutaEntidadOptional.isPresent() && trenEntidadOptional.isPresent()){
+            RutaEntidad rutaEntidad = rutaEntidadOptional.get();
+            TrenEntidad trenEntidad = trenEntidadOptional.get();
+            //asighnar a sus ID
+            horarioEntidad.setId_ruta(rutaEntidad);
+            horarioEntidad.setId_tren(trenEntidad);
+            //guardar
+            HorarioEntidad entidadGuardar = repositorio.save(horarioEntidad);
+            return entidadGuardar;
+        }else{
+            throw new RuntimeException("No se encontró el id ruta con ID: " + id_ruta + "Id tren:   "+  id_tren);
+
+        }
+    }
+
 
 
 
@@ -31,4 +62,31 @@ public class HorarioService {
     public void eliminar(Long id_horario){
         repositorio.deleteById(id_horario);
     }
+
+    //metodo para buscarPorSuId
+    public HorarioEntidad buscarPorId(Long id_horario){
+        HorarioEntidad horarioEntidad = this.repositorio.findById(id_horario).orElse(null);
+        return horarioEntidad;
+    }
+
+    //metodo para editar
+    public HorarioEntidad editar(HorarioEntidad horarioEntidad, Long id_horario){
+        //validar si existe
+        HorarioEntidad horarioExiste = buscarPorId(id_horario);
+        if(horarioExiste !=null){
+            //actualiza los campos
+            horarioExiste.setDias_semana(horarioEntidad.getDias_semana());
+            horarioExiste.setHora_salida(horarioEntidad.getHora_salida());
+            horarioExiste.setHora_llegada(horarioEntidad.getHora_llegada());
+            return repositorio.save(horarioExiste);
+        }else{
+            throw new RuntimeException("No se encontró el horario con ID: " + id_horario);
+
+        }
+
+    }
+
+
+
+
 }
