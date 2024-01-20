9b2619c7c7402765375bd7d717382b8b40b0c0b0
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-19 19:57:29 -0500

    Update TrenService.java

--- backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/servicios/TrenService.java
+++ backend_docker_postgres_springBoot/src/main/java/com/example/backend_docker_postgres_springboot/servicios/TrenService.java
@@ -1,12 +1,14 @@
 package com.example.backend_docker_postgres_springboot.servicios;
 
 import com.example.backend_docker_postgres_springboot.entidades.EstacionEntidad;
+import com.example.backend_docker_postgres_springboot.entidades.ReservaEntidad;
 import com.example.backend_docker_postgres_springboot.entidades.TrenEntidad;
 import com.example.backend_docker_postgres_springboot.repositorios.EstacionRepositorio;
 import com.example.backend_docker_postgres_springboot.repositorios.TrenRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -33,5 +35,43 @@ public class TrenService {
         }else{
             throw new RuntimeException("No se encontró el usuario con ID: " + id_estacion);
         }
+    }
+
+    //metodo para editar
+    public TrenEntidad editarTren(TrenEntidad trenEntidad,
+                                  Long id_tren){
+        //verificar que existe el id del tren
+        TrenEntidad trenExiste = buscarPorId(id_tren);
+        if(trenExiste !=null){
+            //actualizar los campos
+            trenExiste.setModelo_tren(trenEntidad.getModelo_tren());
+            trenExiste.setCapacidad(trenEntidad.getCapacidad());
+            trenExiste.setEstado(trenEntidad.getEstado());
+            //guardar
+            return  repositorio.save(trenExiste);
+        }else{
+            throw new RuntimeException("No se encontró la reserva con ID: " + id_tren);
+
+        }
+    }
+
+    public TrenEntidad buscarPorId(Long id_tren){
+        TrenEntidad trenes = this.repositorio.findById(id_tren).orElse(null);
+        return trenes;
+    }
+
+    //obtener por id
+    public Optional<TrenEntidad> encontrarPorId(Long id_tren){
+        return repositorio.findById(id_tren);
+    }
+
+    //listar
+    public List<TrenEntidad> listar(){
+        return repositorio.findAll();
+    }
+
+    //eliminar por id
+    public void eliminar(Long id_tren){
+        repositorio.deleteById(id_tren);
     }
 }
